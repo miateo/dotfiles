1fc7c1fe44ed942ffba46fbf1dc666ffca243b10
miateo <miateo17@gmail.com>
2024-06-11 16:01:06 +0200

    Update to last change

--- nvim/lua/miateo/plugins/init.lua
+++ nvim/lua/miateo/plugins/init.lua
@@ -1,4 +1,10 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  }
 }
--- nvim/lua/miateo/plugins/telescope.lua
+++ nvim/lua/miateo/plugins/telescope.lua
@@ -11,6 +11,7 @@ return {
     local telescope = require("telescope")
     local actions = require("telescope.actions")
 
+
     telescope.setup({
       defaults = {
         path_display = { "smart" },
@@ -25,6 +26,7 @@ return {
     })
 
     telescope.load_extension("fzf")
+    local builtin = require('telescope.builtin')
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
@@ -32,6 +34,7 @@ return {
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Search though buffers" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
   end,
